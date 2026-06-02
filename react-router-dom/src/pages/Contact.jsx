069f0a2cdda00bacdff0c3943d72dd3a58a4a68b
DeveloperNitesh-Rdhi72 <niteshdeveloper71@gmail.com>
2026-06-02 09:41:24 +0530

    react-router-dom

---
+++ react-router-dom/src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='flex items-center justify-center h-screen'>
+        <h1 className="text-5xl font-bold">Contact page</h1>
+    </div>
+  )
+}
+
+export default Contact
---
+++ react-router-dom/src/pages/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Products = () => {
+  return (
+    <div className='flex items-center justify-center h-screen'>
+        <h1 className="text-5xl font-bold">Products page</h1>
+    </div>
+  )
+}
+
+export default Products
